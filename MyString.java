727e61c15af5805af152edcef68de79aa51255b8
NoaOz <noa.oz01@post.runi.ac.il>
2024-12-04 01:10:35 +0200

    Primes completed fixed

--- MyString.java
+++ MyString.java
@@ -5,7 +5,6 @@ public class MyString {
         System.out.println("This costs 15 Sheksls : " + lowerCase("This costs 15 Sheksls"));
         System.out.println("TLV : " + lowerCase("TLV"));
         System.out.println("lowercase : " + lowerCase("lowercase"));
-
         System.out.println("Testing contains:");
         System.out.println(contains("unhappy", "happy")); // true
         System.out.println(contains("happy", "unhappy")); // false
@@ -33,23 +32,23 @@ public class MyString {
         return LowStr;
     }
 
-    /** If str1 contains str2, returns true; otherwise returns false. */ // use break
+    /** If str1 contains str2, returns true; otherwise returns false. */ 
     public static boolean contains(String str1, String str2) {
-        if (str1.length() >= str2.length()){
-            for (int i = 0; i < str1.length(); i++) {
-                if (str1.charAt(i) == str2.charAt(0)){
-                    for (int j = 1; j < str2.length(); j++) {
-                        if (str1.charAt(i+j) != str2.charAt(j)){
-                            return false;
-                        }
-                    }
-                return true;
+        if (str1.length() < str2.length()) {
+            return false;
+        }
+        for (int i = 0; i < str1.length(); i++) {
+            boolean Isfound = true;
+            for (int j = 0; j < str2.length(); j++) {
+                if (str1.charAt(i + j) != str2.charAt(j)) {
+                    Isfound = false;
+                    break;
                 }
-            
+            }
+            if (Isfound) {
+                return true;
             }
         }
-    return false;    
-  
-
+        return false;
     }
 }

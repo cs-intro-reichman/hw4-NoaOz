e26d0867060be5afbbe619aa963f723a85488f51
NoaOz <noa.oz01@post.runi.ac.il>
2024-12-03 23:34:28 +0200

    Completed my string

--- MyString.java
+++ MyString.java
@@ -18,13 +18,38 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String LowStr = "";
+        int i = 0;
+        while (i < str.length()) {
+            char ch = str.charAt(i);
+            if (ch >= 65 && ch <= 90) {
+                LowStr = LowStr + (char) (ch +32);
+            }
+            else {
+                LowStr = LowStr + ch; 
+            }
+            i++;
+        }
+        return LowStr;
     }
 
-    /** If str1 contains str2, returns true; otherwise returns false. */
+    /** If str1 contains str2, returns true; otherwise returns false. */ // use break
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+        if (str1.length() >= str2.length()){
+            for (int i = 0; i < str1.length(); i++) {
+                if (str1.charAt(i) == str2.charAt(0)){
+                    for (int j = 1; j < str2.length(); j++) {
+                        if (str1.charAt(i+j) != str2.charAt(j)){
+                            return false;
+                        }
+                    }
+                return true;
+                }
+            
+            }
+        }
+    return false;    
+  
+
     }
 }
